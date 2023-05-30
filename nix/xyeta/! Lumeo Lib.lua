82296b104e577fee599d74e39c85352248ede26e
w1ndyxz <83566386+w1ndyxz@users.noreply.github.com>
2023-05-31 01:04:35 +1000

    Create ! Lumeo Lib.lua

---
+++ nix/xyeta/! Lumeo Lib.lua
@@ -0,0 +1,100 @@
+all_models = {
+    terrorist = {
+        {"Chiken", "models/chicken/chicken.mdl"},
+
+        {"SABRE | Dragomir", "models/player/custom_player/legacy/tm_balkan_variantl.mdl"},
+        {"SABRE | Dragomir F", "models/player/custom_player/legacy/tm_balkan_variantf.mdl"},
+        {"SABRE | Maximus", "models/player/custom_player/legacy/tm_balkan_varianti.mdl"},
+        {"SABRE | Blackwolf", "models/player/custom_player/legacy/tm_balkan_variantj.mdl"},
+        {"SABRE | Rezan Ready", "models/player/custom_player/legacy/tm_balkan_variantg.mdl"},
+        {"SABRE | Rezan Redshirt", "models/player/custom_player/legacy/tm_balkan_variantk.mdl"},
+        {"SABRE | Doc. Romanov", "models/player/custom_player/legacy/tm_balkan_varianth.mdl"},
+
+        {"ELITE | Jungle Rebel", "models/player/custom_player/legacy/tm_leet_variantj.mdl"},
+        {"ELITE | Mr. Muhlik", "models/player/custom_player/legacy/tm_leet_variantf.mdl"},
+        {"ELITE | Ground Rebel", "models/player/custom_player/legacy/tm_leet_variantg.mdl"},
+        {"ELITE | Osiris", "models/player/custom_player/legacy/tm_leet_varianth.mdl"},
+        {"ELITE | Pro. Shahmat", "models/player/custom_player/legacy/tm_leet_varianti.mdl"},
+
+        {"GUER | Aggressor", "models/player/custom_player/legacy/tm_jungle_raider_variantf.mdl"},
+        {"GUER | Col. Mangos", "models/player/custom_player/legacy/tm_jungle_raider_variantd.mdl"},
+        {"GUER | Trapper", "models/player/custom_player/legacy/tm_jungle_raider_variantf2.mdl"},
+        {"GUER | Solman", "models/player/custom_player/legacy/tm_jungle_raider_varianta.mdl"},
+        {"GUER | Arno Overgrown", "models/player/custom_player/legacy/tm_jungle_raider_variantc.mdl"},
+        {"GUER | Vypa Sista", "models/player/custom_player/legacy/tm_jungle_raider_variante.mdl"},
+        {"GUER | 'Medium R' Cras.", "models/player/custom_player/legacy/tm_jungle_raider_variantb2.mdl"},
+        {"GUER | Cras. Forgotten", "models/player/custom_player/legacy/tm_jungle_raider_variantb.mdl"},
+
+        {"PHOENIX | Enforcer", "models/player/custom_player/legacy/tm_phoenix_variantf.mdl"},
+        {"PHOENIX | Soldier", "models/player/custom_player/legacy/tm_phoenix_varianth.mdl"},
+        {"PHOENIX | Street Soldier", "models/player/custom_player/legacy/tm_phoenix_varianti.mdl"},
+        {"PHOENIX | Slingshot", "models/player/custom_player/legacy/tm_phoenix_variantg.mdl"},
+
+        {"PRO | Getaway Sally", "models/player/custom_player/legacy/tm_professional_varj.mdl"},
+        {"PRO | Little Kev", "models/player/custom_player/legacy/tm_professional_varh.mdl"},
+        {"PRO | Number K", "models/player/custom_player/legacy/tm_professional_vari.mdl"},
+        {"PRO | Saf. Voltzmann", "models/player/custom_player/legacy/tm_professional_varg.mdl"},
+        {"PRO | Bloody D", "models/player/custom_player/legacy/tm_professional_varf5.mdl"},
+        {"PRO | Miami D", "models/player/custom_player/legacy/tm_professional_varf.mdl"},
+        {"PRO | Silent D", "models/player/custom_player/legacy/tm_professional_varf1.mdl"},
+        {"PRO | Skullhead D", "models/player/custom_player/legacy/tm_professional_varf2.mdl"},
+        {"PRO | Royale D", "models/player/custom_player/legacy/tm_professional_varf3.mdl"},
+        {"PRO | Loudmouth D", "models/player/custom_player/legacy/tm_professional_varf4.mdl"},
+    },
+    ct = {
+        {"Chiken", "models/chicken/chicken.mdl"},
+
+        {"BRAZ | Primeiro Tenente", "models/player/custom_player/legacy/ctm_st6_variantn.mdl"},
+
+        {"KSK | 3rd Commando Company", "models/player/custom_player/legacy/ctm_st6_variantk.mdl"},
+
+        {"SAS | B Squadron", "models/player/custom_player/legacy/ctm_sas_variantf.mdl"},
+        {"SAS | D Squadron", "models/player/custom_player/legacy/ctm_sas_variantg.mdl"},
+
+        {"FROG | Rex Krikey", "models/player/custom_player/legacy/ctm_diver_variantc.mdl"},
+        {"FROG | Wet Sox", "models/player/custom_player/legacy/ctm_diver_variantb.mdl"},
+        {"FROG | Goggles", "models/player/custom_player/legacy/ctm_diver_varianta.mdl"},
+
+        {"GENDAR | Aspirant", "models/player/custom_player/legacy/ctm_gendarmerie_variantd.mdl"},
+        {"GENDAR | Jacques Beltram", "models/player/custom_player/legacy/ctm_gendarmerie_variante.mdl"},
+        {"GENDAR | Sous-Lieutenant", "models/player/custom_player/legacy/ctm_gendarmerie_varianta.mdl"},
+        {"GENDAR | Chem-Haz", "models/player/custom_player/legacy/ctm_gendarmerie_variantb.mdl"},
+        {"GENDAR | Escadron Rouchard", "models/player/custom_player/legacy/ctm_gendarmerie_variantc.mdl"},
+
+        {"NSWC | Seal Team 6", "models/player/custom_player/legacy/ctm_st6_variante.mdl"},
+        {"NSWC | Buckshot", "models/player/custom_player/legacy/ctm_st6_variantg.mdl"},
+        {"NSWC | Blueberries", "models/player/custom_player/legacy/ctm_st6_variantj.mdl"},
+        {"NSWC | Ricksaw", "models/player/custom_player/legacy/ctm_st6_varianti.mdl"},
+
+        {"FBI | Operator", "models/player/custom_player/legacy/ctm_fbi_variantf.mdl"},
+        {"FBI | Markus Delrow", "models/player/custom_player/legacy/ctm_fbi_variantg.mdl"},
+        {"FBI | Agent Ava", "models/player/custom_player/legacy/ctm_fbi_variantb.mdl"},
+        {"FBI | Michael Syfers", "models/player/custom_player/legacy/ctm_fbi_varianth.mdl"},
+
+        {"SWAT | Chem-Haz", "models/player/custom_player/legacy/ctm_swat_variantj.mdl"},
+        {"SWAT | Bio-Haz", "models/player/custom_player/legacy/ctm_swat_varianth.mdl"},
+        {"SWAT | Tree Hugger", "models/player/custom_player/legacy/ctm_swat_variantk.mdl"},
+        {"SWAT | Van Healen", "models/player/custom_player/legacy/ctm_swat_variantg.mdl"},
+        {"SWAT | Sergeant Bombson", "models/player/custom_player/legacy/ctm_swat_varianti.mdl"},
+        {"SWAT | Farlow", "models/player/custom_player/legacy/ctm_swat_variantf.mdl"},
+        {"SWAT | Dead Cold", "models/player/custom_player/legacy/ctm_swat_variante.mdl"},
+
+        {"TACP | McCoy M", "models/player/custom_player/legacy/ctm_st6_variantm.mdl"},
+        {"TACP | McCoy L", "models/player/custom_player/legacy/ctm_st6_variantl.mdl"},
+    },
+}
+
+
+-- На данный момент не работает
+custom_say = {
+    kill = {
+        "Пример сообщения после убийства #1",
+        "Пример сообщения после убийства #2",
+        "Example сообщения после убийства #3",
+    },
+    death = {
+        "Пример сообщения после смерти #1",
+        "Пример сообщения после смерти #2",
+        "Example сообщения после смерти #3",
+    }
+}
