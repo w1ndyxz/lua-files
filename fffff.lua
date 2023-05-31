a97a26a06108871813f9f6a04d2989a4218346db
w1ndyxz <83566386+w1ndyxz@users.noreply.github.com>
2023-06-01 01:36:52 +1000

    Add files via upload

---
+++ fffff.lua
@@ -0,0 +1,42 @@
+--[[
+▄▄▄ . ▌ ▐· ▄▄▄· ▄▄▌  ▪  ▄▄▄▄· 
+▀▄.▀·▪█·█▌▐█ ▀█ ██•  ██ ▐█ ▀█▪
+▐▀▀▪▄▐█▐█•▄█▀▀█ ██▪  ▐█·▐█▀▀▀▄
+▐█▄▄▌ ███ ▐█ ▪▐▌▐█▌▐▌▐█▌██▄▪▐█
+ ▀▀▀ . ▀   ▀  ▀ .▀▀▀ ▀▀▀·▀▀▀▀ 
+Cleverly done, Mr. Freeman, but you're not supposed to be here. 
+As a matter of fact, you're not. 
+Get back where you belong, and forget about all this, until we meet again.
+https://static.wikia.nocookie.net/halflife/images/c/c9/Gman_12.ogg/revision/latest?cb=20210516190003&path-prefix=ru
+]]--
+
+evalib = {
+    next_update_announcement = {
+        beta = "01.06.23",
+        prem = "10.06.23",
+        free = "25.06.23",
+    },
+
+    last_update = {
+        beta = "02.05.23",
+        prem = "02.05.23",
+        free = "08.05.23",
+    },
+    
+    current_version = {
+        beta = "9",
+        prem = "8.1",
+        free = "8.1",
+    },
+ 
+    key_check = {
+        beta = "zxc",
+        prem = "zxc",
+    },
+
+    pivate_key = {
+        beta = "b",
+        prem = "p",
+        free = "f",
+    },
+}
